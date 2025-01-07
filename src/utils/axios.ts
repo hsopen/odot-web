53529e39525c410118dc352f17e260eba6f8e76e
hsopen <hsopen@outlook.com>
2025-01-07 23:17:51 +0800

    ✨ feat: 添加axios拦截器
    添加自动将cookie附加到请求中

---
+++ src/utils/axios.ts
@@ -0,0 +1,24 @@
+// src/utils/axios.js
+import axios from 'axios'
+import Cookies from 'js-cookie' // 引入 js-cookie
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_HOST, // 你的后端 API 地址
+  withCredentials: true, // 允许携带 Cookie
+})
+
+// 请求拦截器
+instance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get('token') // 获取 token
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}` // 设置 Authorization 头
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+export default instance
